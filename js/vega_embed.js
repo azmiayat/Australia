da5ff9c43bdf82926916e7ec63983ff23e76efd1
Azmi Ayat <azmiayat744@gmail.com>
2026-05-18 15:53:39 +0600

    hi

--- js/vega_embed.js
+++ js/vega_embed.js
@@ -1,7 +1,6 @@
-vegaEmbed("#top-countries-bar", "./visualisations/top_countries_bar.vg.json").catch(console.error);
+vegaEmbed("#top-countries-bar", "visualisations/top_countries_bar.vg.json").catch(console.error);
 
-// Add this later after creating yearly_trend_line.vg.json
-// vegaEmbed("#yearly-trend-line", "./visualisations/yearly_trend_line.vg.json").catch(console.error);
+vegaEmbed("#yearly-trend-line", "visualisations/yearly_trend_line.vg.json").catch(console.error);
 
-// Add this later after creating world_map.vg.json
-// vegaEmbed("#world-map", "./visualisations/world_map.vg.json").catch(console.error);
+// We will add this after creating the map spec.
+// vegaEmbed("#world-map", "visualisations/world_map.vg.json").catch(console.error);
